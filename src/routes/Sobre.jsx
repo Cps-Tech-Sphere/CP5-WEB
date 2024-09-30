da76aa8c6a2034503a7682c48f4588fb91429dd8
Jean <jeanmatheus0612@gmail.com>
2024-09-30 14:25:54 -0300

    Estruturando a página sobre nós

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,8 +1,95 @@
-const Sobre =()=>{
+import { SobreStyle } from "../style/SobreStyle";
+import imagemtema from "../assets/Imagens/img/imagemtema.png";
+
+const Sobre = () => {
 
     return (
-        <></>
-    )
-}
+        <SobreStyle>
+            <section className="imagem-tema">
+                <img src={imagemtema} alt="imagem tema" />
+            </section>
 
-export default Sobre
+            <section className="sobrenos">
+                <h1>Sobre Nós</h1>
+                <p>
+                Na E-Veloz, nossa missão é redefinir a mobilidade urbana por meio de
+                soluções sustentáveis e tecnológicas. Acreditamos que o futuro do
+                transporte está nas bicicletas elétricas, que combinam eficiência,
+                conveniência e respeito ao meio ambiente. Queremos oferecer ao nosso
+                público uma experiência única, com produtos de alta qualidade que unem
+                inovação e design.
+                </p>
+            </section>
+
+            <section className="container-integrantes">
+                <ul className="lista-integrantes">
+                    <li className="item-integrantes">
+                        <img src="https://via.placeholder.com/150" alt="Foto Integrante 1" />
+                        <div>
+                        <h1>Ianny Raquel</h1>
+                        <p>
+                            Com uma sólida experiência em design de interfaces, Ianny é
+                            apaixonada por criar experiências que conectam tecnologia e
+                            usabilidade. Na E-Veloz, ela traz seu olhar afiado para garantir
+                            que a jornada do cliente seja simples e agradável.
+                        </p>
+                        </div>
+                    </li>
+
+                    <li className="item-integrantes">
+                        <img src="https://via.placeholder.com/150" alt="Foto Integrante 2" />
+                        <div>
+                        <h1>Jean Matheus</h1>
+                        <p>
+                            Jean é movido por inovação e eficiência. Sua habilidade em
+                            desenvolvimento de interfaces ajuda a E-Veloz a sempre estar na
+                            vanguarda das soluções digitais para o e-commerce.
+                        </p>
+                        </div>
+                    </li>
+
+                    <li className="item-integrantes">
+                        <img src="https://via.placeholder.com/150" alt="Foto Integrante 3" />
+                        <div>
+                        <h1>João Victor Soave</h1>
+                        <p>
+                            Com uma forte liderança no gerenciamento de projetos, João traz
+                            sua expertise em processos ágeis e otimização de recursos,
+                            garantindo que as operações da E-Veloz sejam fluidas e bem
+                            organizadas.
+                        </p>
+                        </div>
+                    </li>
+
+                    <li className="item-integrantes">
+                        <img src="https://via.placeholder.com/150" alt="Foto Integrante 4" />
+                        <div>
+                        <h1>Maria Alice</h1>
+                        <p>
+                            Líder no desenvolvimento de Front-End, Maria combina sua paixão
+                            por inovação com um profundo conhecimento de tecnologia,
+                            assegurando que o design e a funcionalidade da loja virtual
+                            estejam sempre alinhados com as melhores práticas do mercado.
+                        </p>
+                        </div>
+                    </li>
+
+                    <li className="item-integrantes">
+                        <img src="https://via.placeholder.com/150" alt="Foto Integrante 5" />
+                        <div>
+                        <h1>Thiago Oliveira</h1>
+                        <p>
+                            Como Product Owner, Thiago é o responsável por garantir que a
+                            E-Veloz esteja sempre em sintonia com as necessidades dos
+                            consumidores, liderando o time com visão estratégica e foco nos
+                            resultados.
+                        </p>
+                        </div>
+                    </li>
+                </ul>
+            </section>
+        </SobreStyle>
+    );
+};
+
+export default Sobre;
---
+++ src/style/SobreStyle.js
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+export const SobreStyle = styled.main`
+
+    
+
+
+    .imagem-tema img {
+        width: 100%;
+    }
+
+    .sobrenos {
+        margin: 55px;
+    }
+
+    .sobrenos h1 {
+        font-size: 2.5rem;
+        color: #C7EA46;
+    }
+
+    .sobrenos p {
+        font-size: 1.2rem;
+        color: #1E1E1E;
+        padding: 10px 0 50px 0;
+    }
+
+    .container-integrantes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh; 
+        margin-top: 40px;
+        margin-bottom: 40px;
+    }
+
+    .lista-integrantes {
+        list-style: none;
+        padding: 0;
+    }
+
+    .item-integrantes {
+        display: flex;
+        align-items: center;
+        justify-content: center; 
+        margin-bottom: 30px;
+    }
+
+    .item-integrantes img {
+        width: 150px;
+        height: 150px;
+        object-fit: cover;
+        margin-right: 20px;
+        border-radius: 50%;
+    }
+
+    .item-integrantes h1 {
+        font-size: 1.8rem;
+        color: #C7EA46;
+        margin: 0;
+        padding-bottom: 10px;
+    }
+
+    .item-integrantes p {
+        font-size: 1.2rem;
+        color: #1E1E1E;
+    }
+
+    .item-integrantes div {
+        max-width: 600px;
+    }
+`;
